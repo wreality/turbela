ad92433cdf24395d407bbbb2baec05560873da29
Brian Adams <wreality@gmail.com>
2023-08-27 22:56:51 -0400

    compoer updates

--- backend/app/Http/Middleware/TrustProxies.php
+++ backend/app/Http/Middleware/TrustProxies.php
@@ -1,8 +1,7 @@
 <?php
 
 namespace App\Http\Middleware;
-
-use Fideloper\Proxy\TrustProxies as Middleware;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
