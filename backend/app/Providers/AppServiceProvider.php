6e4392015f0c5ed38757090ca7460721f0152a1d
Brian Adams <wreality@gmail.com>
2023-08-30 15:05:26 -0400

    force https

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        \URL::forceScheme('https');
         try {
 
             $paymentSettings = app(PaymentSettings::class);
